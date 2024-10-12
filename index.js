57e6cda53fb55bdf8c4d71b39712b927d02500d2
RBenOthmen <ramibenothmen15@gmail.com>
2024-10-12 22:31:53 +0100

    Added Select by Regular Expressions

--- index.js
+++ index.js
@@ -30,11 +30,14 @@ async function createCourse() {
 async function getCourses() {
     const courses = await Course
         // .find({ author: 'Mosh Hamedani', isPublished: true })
-        // .find({ price: { $gte: 10, $lte: 20 } })
-        // .find({ price: { $in: [10, 15, 20] } })
-        .find()
-        .or([{ author: 'Mosh Hamedani' }, { isPublished: true }])
-        .and([])
+        // Starts with
+        .find({ author: /^Mosh/ })
+        // Ends with
+        .find({ author: /Hamedani$/ })
+        // Ends with / Case incensitive
+        .find({ author: /Hamedani$/i })
+        // Contains
+        .find({ author: /.*Mosh*./ })
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1 });
