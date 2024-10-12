aa3b8ebb481e455f11cf58ab4b0e9d7be6e84f6b
RBenOthmen <ramibenothmen15@gmail.com>
2024-10-12 22:22:50 +0100

    Used Logical Query Operators 'OR' & 'AND'

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ async function getCourses() {
     const courses = await Course
         // .find({ author: 'Mosh Hamedani', isPublished: true })
         // .find({ price: { $gte: 10, $lte: 20 } })
-        .find({ price: { $in: [10, 15, 20] } })
+        // .find({ price: { $in: [10, 15, 20] } })
+        .find()
+        .or([{ author: 'Mosh Hamedani' }, { isPublished: true }])
+        .and([])
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1 });
