9d7d7e4e73124585fd3d6e67fe28101c296be840
RBenOthmen <ramibenothmen15@gmail.com>
2024-10-12 19:10:16 +0100

    Queried documents

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Angular Course',
+        name: 'Django Course',
         author: 'Mosh Hamedani',
-        tags: ['angular', 'frontend'],
+        tags: ['django', 'backend'],
         isPublished: true,
     });
 
@@ -27,4 +27,13 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Mosh Hamedani', isPublished: true })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 });
+    console.log(courses);
+}
+
+getCourses();
